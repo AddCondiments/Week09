a5051220d536b0192626d347acdcc051632b857d
AddCondiments <addcondiments@gmail.com>
2017-07-11 16:01:25 -0400

    Well shit... I screwed up.

---
+++ Week09/SuperHuman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week09
+{
+    public class SuperHuman : Human
+    {
+        // Private Fields
+        public List<>
+
+            /// <summary>
+            /// Cannot continue, missin the "power" datatype
+            /// </summary>
+            /// <param name="name"></param>
+
+        // Constructors
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        // Private Mthods
+        private void _initalize()
+        {
+            this
+        }
+
+        // Public Methods
+    }
+}
