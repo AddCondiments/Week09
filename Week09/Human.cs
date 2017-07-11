f3fa59aa42a26da11e8521fc74c9d28058599e7e
AddCondiments <addcondiments@gmail.com>
2017-07-11 15:27:34 -0400

    Added an abstract class called "Human".

---
+++ Week09/Human.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Name: Matthew Watada
+ * July 11th 2017
+ * Description: This is the abstract class
+ * Version: 0.1 Initial Commit
+namespace Week09
+{
+    public abstract class Human
+    {
+        // Private Fields
+        private string _name;
+        // Public Properties --------------------------------------
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+            //Constructors
+
+            public Human(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
+*/
